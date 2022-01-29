8e1d21f606412aea83e6f1e915259dbd9ee0a40d
mishamsk <mishamsk@gmail.com>
2022-01-28 22:53:02 -0500

    ✨ encryption now supports init_token
    To be used for known users

--- nerddiary/core/data/crypto.py
+++ nerddiary/core/data/crypto.py
@@ -24,13 +24,45 @@ def _derive_key(password: bytes, salt: bytes, iterations: int = ITERATIONS) -> b
 
 
 class EncryptionProdiver:
-    def __init__(self, password: str, iterations: int = ITERATIONS) -> None:
-        self._password = password
-        self._iterations = iterations
-        self._salt = secrets.token_bytes(16)
-        self._key = _derive_key(password.encode(), self._salt, self._iterations)
+    def __init__(self, password: str, init_token: bytes = None, control_message: bytes = None) -> None:
+        """Inits a new Encryption provider. Encrypts messages into a (salt, iterations, encrypted_message) tokens. Decrypts such tokens.
+
+        If no `init_token` is given, a new salt is generated, standard iterations value is set and a new key is derived from a given password.
+
+        If `init_token` is provided it is decrypted using the given password. Throws `cryptography.fernet.InvalidToken` if it failed (meaning the password is incorrect)
+
+        If a `control_message` is given, the decrypted message is compared to `control_message`. In case of mismatch a `ValueError` is raised"""
+
+        salt = iterations = key = None
+
+        if init_token is not None:
+            decoded = b64d(init_token)
+            salt, iter, token = decoded[:16], decoded[16:20], b64e(decoded[20:])
+            iterations = int.from_bytes(iter, "big")
+            key = _derive_key(password.encode(), salt, iterations)
+            init_message = Fernet(key).decrypt(token)
+
+            if control_message is not None and control_message != init_message:
+                raise ValueError("Init message didn't match control bytes")
+
+        self._iterations = iterations or ITERATIONS
+        self._salt = salt or secrets.token_bytes(16)
+        self._key = key or _derive_key(password.encode(), self._salt, self._iterations)
+
+    @property
+    def salt(self) -> bytes:
+        return self._salt
+
+    @property
+    def iterations(self) -> int:
+        return self._iterations
+
+    @property
+    def key(self) -> bytes:
+        return self._key
 
     def encrypt(self, message: bytes) -> bytes:
+        """Encrypts message and stores it alongside the salt & iterations"""
         return b64e(
             b"%b%b%b"
             % (
@@ -41,11 +73,16 @@ class EncryptionProdiver:
         )
 
     def decrypt(self, token: bytes) -> bytes:
+        """Validates the message has the same salt & iterations stored and then decrypts the message.
+
+        Throws `ValueError` if salt or iterations mismatch, which means that the password used to derive the key may have been correct, but the sale/iterations used were wrong
+
+        Throws `cryptography.fernet.InvalidToken` if salt and iterations match but decryption failed (meaning somehow the correct salt/iteration was given during initialization but the password is incorrect)
+        """
+
         decoded = b64d(token)
         salt, iter, token = decoded[:16], decoded[16:20], b64e(decoded[20:])
         iterations = int.from_bytes(iter, "big")
         if salt != self._salt or iterations != self._iterations:
-            key = _derive_key(self._password.encode(), salt, iterations)
-        else:
-            key = self._key
-        return Fernet(key).decrypt(token)
+            raise ValueError("Salt or iterations mismatch")
+        return Fernet(self._key).decrypt(token)
--- nerddiary/core/data/test/test_crypto.py
+++ nerddiary/core/data/test/test_crypto.py
@@ -1,5 +1,8 @@
 from nerddiary.core.data.crypto import EncryptionProdiver
 
+import pytest
+from cryptography.fernet import InvalidToken
+
 
 class TestEncryptionProdiver:
     def test_provider(self):
@@ -14,3 +17,31 @@ class TestEncryptionProdiver:
 
         assert encrypted != test_data
         assert decrypted == test_data
+
+        # Test decrypting with the same password, but new provider (new salt & token)
+        encr2 = EncryptionProdiver(password)
+        with pytest.raises(ValueError) as err:
+            encr2.decrypt(encrypted)
+        assert err.type == ValueError
+
+        # Test initialising new provider with init_token allows to decrypt old message
+        encr = EncryptionProdiver(password, init_token=encrypted)
+        decrypted = encr.decrypt(encrypted)
+
+        assert decrypted == test_data
+
+        # Test initialising new provider with init_token & control_message
+        encr = EncryptionProdiver(password, init_token=encrypted, control_message=test_data)
+        decrypted = encr.decrypt(encrypted)
+
+        assert decrypted == test_data
+
+        # Test initialising new provider with init_token & incorrect control_message
+        with pytest.raises(ValueError) as err:
+            encr = EncryptionProdiver(password, init_token=encrypted, control_message=b"wrong data")
+        assert err.type == ValueError
+
+        # Test initialising new provider with init_token & incorrect password
+        with pytest.raises(InvalidToken) as err:
+            encr = EncryptionProdiver("wrong password", init_token=encrypted, control_message=test_data)
+        assert err.type == InvalidToken
