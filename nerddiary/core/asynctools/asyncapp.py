e4fa80fd459161ded72e82c90a949d02bbcd6f16
mishamsk <mishamsk@gmail.com>
2022-02-06 23:22:09 -0500

    ✨ async app abstract class
    Works as regular and async context manager. Provides optional `run()` method similar to asyncio.run()

---
+++ nerddiary/core/asynctools/asyncapp.py
@@ -0,0 +1,287 @@
+from __future__ import annotations
+
+import abc
+import asyncio
+import logging
+import signal
+from time import sleep
+
+from typing import Any, Awaitable, TypeVar
+
+# from .delayedsignal import DelayedKeyboardInterrupt
+
+
+_T = TypeVar("_T")
+_A = TypeVar("_A", bound="AsyncApplication")
+
+
+class AsyncApplication(abc.ABC):
+    def __init__(
+        self,
+        *,
+        loop: asyncio.AbstractEventLoop = None,
+        logger: logging.Logger = None,
+    ):
+        self._loop: asyncio.AbstractEventLoop | None = loop
+        self._logger = logger or logging.getLogger(self.__class__.__name__)
+        self._loop_started_by_self = False
+        self._closed = True
+
+    @property
+    def loop(self) -> asyncio.AbstractEventLoop:
+        if self._loop and not self._loop.is_closed():
+            return self._loop
+
+        try:
+            self._loop = asyncio.get_running_loop()
+        except RuntimeError:
+            self._loop = asyncio.new_event_loop()
+            self._loop_started_by_self = True
+
+        return self._loop
+
+    @property
+    def closed(self) -> bool:
+        return self._closed
+
+    def run(self) -> Any:
+        res = None
+
+        try:
+            self._logger.debug("Executing app startup sequence")
+            if self.start() is not None:
+                self._logger.debug("Running the app")
+                res = self._run_coro(self._arun())
+        except BaseException as e:
+            self._logger.error("Unhandled exception while running the app.\n" + str(e))
+        finally:
+            self._logger.debug("Closing the app")
+            self.close()
+
+        return res
+
+    @abc.abstractmethod
+    async def _astart(self):
+        pass
+
+    @abc.abstractmethod
+    async def _aclose(self) -> bool:
+        pass
+
+    async def _arun(self) -> Any:
+        raise NotImplementedError()
+
+    async def astart(self: _A) -> _A:
+        self._logger.debug(
+            "Running <_astart> method, ensuring <aclose> is executed if SIGINT is sent before <_astart> is finished"
+        )
+
+        func_task: asyncio.Task | None = None
+        hit_sigint = False
+
+        def __handler(sig, frame):
+            nonlocal hit_sigint
+            hit_sigint = True
+
+            self._logger.debug("SIGINT received while runnning <_astart>; cancelling immediately")
+            if func_task:
+                func_task.cancel()
+
+        old_sigint_handler = None
+
+        try:
+            self._logger.debug("astart: Replacing SIGINT handler")
+            old_sigint_handler = signal.signal(signal.SIGINT, __handler)
+
+            func_task = self.loop.create_task(self._astart())
+
+            # Set this even before start, in case start was interuppted but some cleaning is still necessary. Close method must be clever enough to check that object to be cleaned exist
+            self._closed = False
+
+            await func_task
+        except asyncio.CancelledError:
+            if hit_sigint:
+                self._logger.warn("Closing app after SIGINT")
+            else:
+                self._logger.warn("<_astart> was cancelled. Closing app")
+
+            res = await self.aclose()
+
+            if hit_sigint:
+                self._logger.debug("astart: Restoring SIGINT handler and re-rasing SIGINT")
+                signal.signal(signal.SIGINT, old_sigint_handler)
+                raise KeyboardInterrupt()
+
+            if not res:
+                raise
+        finally:
+            if not hit_sigint:
+                self._logger.debug("astart: Restoring SIGINT handler")
+                signal.signal(signal.SIGINT, old_sigint_handler)
+
+        return self
+
+    async def aclose(self) -> bool:
+        if self.closed:
+            self._logger.debug("aclose: App is already closed or hasn't been started. Skipping")
+            return True
+
+        self._logger.debug("Running <_aclose> method, shielding it from SIGINT and cancellation")
+
+        def __handler(sig, frame):
+            self._logger.debug("SIGINT received while runnning <_aclose>; delaying")
+
+        old_sigint_handler = None
+
+        try:
+            self._logger.debug("aclose: Replacing SIGINT handler")
+            old_sigint_handler = signal.signal(signal.SIGINT, __handler)
+
+            res = await asyncio.shield(self._aclose())
+        finally:
+            self._logger.debug("aclose: Restoring SIGINT handler")
+            signal.signal(signal.SIGINT, old_sigint_handler)
+
+        if res is True:
+            self._closed = True
+            return True
+        else:
+            return False
+
+    def start(self: _A) -> _A:
+        return self.loop.run_until_complete(self.astart())
+
+    def close(self) -> bool:
+        res = self.loop.run_until_complete(self.aclose())
+
+        if self._loop_started_by_self:
+            # If the loop was started with AsyncApplication.run() mimic asyncio.run() behavior
+            self._logger.warning(
+                "The loop was started with AsyncApplication.run(), mimicing asyncio.run() behavior and force cancelling all tasks and closing the loop"
+            )
+            try:
+                # Cancel all remaining uncompleted tasks (properly written subclass should never return any).
+                self._cancel_all_tasks()
+
+                # Shutdown all active asynchronous generators.
+                self.loop.run_until_complete(self.loop.shutdown_asyncgens())
+            finally:
+                self._logger.debug("Closing loop")
+                self.loop.close()
+
+        return res
+
+    def _cancel_all_tasks(self):
+        """
+        Cancel all tasks in the loop (code from asyncio.run()).
+        """
+
+        to_cancel = asyncio.tasks.all_tasks(self._loop)
+        self._logger.debug(f"Cancelling all remaining tasks in the loop. A totoal of {len(to_cancel)} tasks")
+
+        if not to_cancel:
+            return
+
+        for task in to_cancel:
+            task.cancel()
+
+        self.loop.run_until_complete(asyncio.tasks.gather(*to_cancel, return_exceptions=True))
+
+        for task in to_cancel:
+            if task.cancelled():
+                continue
+
+            if task.exception() is not None:
+                self.loop.call_exception_handler(
+                    {
+                        "message": f"unhandled exception during {self.__class__}.run() shutdown",
+                        "exception": task.exception(),
+                        "task": task,
+                    }
+                )
+
+    async def _async_sigint_shield(
+        self, func: Awaitable[_T], *, ensure_func_run: bool = False, close: bool = True
+    ) -> _T | None:
+        self._logger.debug(f"Running a sigint shielded class method {func}. {ensure_func_run=};{close=}")
+
+        func_task: asyncio.Task | None = None
+        hit_sigint = False
+
+        def __handler(sig, frame):
+            nonlocal hit_sigint
+            hit_sigint = True
+
+            if ensure_func_run:
+                self._logger.debug("SIGINT received while runnning a shielded method; delaying")
+            else:
+                self._logger.debug("SIGINT received while runnning a shielded method; cancelling {func} immediately")
+                if func_task:
+                    func_task.cancel()
+
+        self._logger.debug("Entering DelayedKeyboardInterrupt context")
+        old_sigint_handler = signal.signal(signal.SIGINT, __handler)
+        try:
+            func_task = self.loop.create_task(func)
+            return await func_task
+        except asyncio.CancelledError:
+            res = True
+
+            if close:
+                self._logger.warn("{func} was cancelled. Closing app")
+                res = await self.aclose()
+
+            if not res:
+                raise
+
+            return None
+        finally:
+            self._logger.debug("Exiting DelayedKeyboardInterrupt context. Restoring signal handlers")
+            signal.signal(signal.SIGINT, old_sigint_handler)
+
+            if hit_sigint:
+                res = True
+
+                if close:
+                    self._logger.warn("Closing app after a delayed SIGINT")
+                    res = await self.aclose()
+
+                if not res:
+                    raise
+
+                return None
+
+    def _run_coro(self, coro: Awaitable[_T]) -> _T:
+        loop = self.loop
+
+        if loop.is_running():
+            self._logger.debug(f"Running coroutine {coro} on an already running loop")
+            task = loop.create_task(coro)
+            while not task.done():
+                sleep(0.1)
+
+            return task.result()
+        else:
+            self._logger.debug(f"Running loop until coroutine {coro} is complete")
+            return self.loop.run_until_complete(coro)
+
+    def __enter__(self: _A) -> _A:
+        self._logger.debug("Entering regular (non-async) context")
+        return self._run_coro(self.__aenter__())
+
+    def __exit__(self, exc_type, exc_value, traceback) -> bool:
+        self._logger.debug("Exiting regular (non-async) context")
+        return self._run_coro(self.__aexit__(exc_type, exc_value, traceback))
+
+    async def __aenter__(self: _A) -> _A:
+        self._logger.debug("Entering async context")
+        return await self.astart()
+
+    async def __aexit__(self, exc_type, exc_value, traceback) -> bool:
+        self._logger.debug("Exiting async context")
+        if exc_type and exc_type != KeyboardInterrupt:
+            self._logger.error(
+                "Exception caught before application was closed", exc_info=(exc_type, exc_value, traceback)
+            )
+
+        return await self.aclose()
--- nerddiary/core/client/client.py
+++ nerddiary/core/client/client.py
@@ -4,7 +4,11 @@ import asyncio
 import json
 import logging
 import uuid
+from contextlib import suppress
 
+from nerddiary.core.asynctools.asyncapp import AsyncApplication
+
+# TODO: change to relative import
 from nerddiary.core.client.config import NerdDiaryClientConfig
 
 from jsonrpcclient.requests import request_uuid
@@ -27,21 +31,46 @@ class AsyncResult:
         return await self._fut
 
 
-@method
-async def ping(p) -> Result:
-    print("Call ndc!")
-    return Success("pong " + str(p))
+class NerdDiaryClient(AsyncApplication):
+    def __init__(
+        self, *, config: NerdDiaryClientConfig = NerdDiaryClientConfig(), loop: asyncio.AbstractEventLoop = None
+    ) -> None:
+        super().__init__(loop=loop, logger=logger)
 
-
-class NerdDiaryClient:
-    def __init__(self, config: NerdDiaryClientConfig = NerdDiaryClientConfig()) -> None:
         self._config = config
-
         self._running = False
         self._connect_lock = asyncio.Lock()
         self._ws: client.WebSocketClientProtocol | None = None
         self._rpc_dispatcher: asyncio.Task | None = None
         self._rpc_calls: t.Dict[uuid.UUID, AsyncResult] = {}
+
+    async def _astart(self):
+        logger.debug("Starting NerdDiary client")
+        self._running = True
+        await self._connect()
+        self._rpc_dispatcher = asyncio.create_task(self._rpc_dispatch())
+
+    async def _aclose(self) -> bool:
+        logger.debug("Closing NerdDiary client")
+        if self._running:
+            # Stop any internal loops
+            self.stop()
+
+        # If rpc dispatcher exist, wait for it to stop
+        if self._rpc_dispatcher and self._rpc_dispatcher.cancel():
+            logger.debug("Waiting for RPC dispatcher to gracefully finish")
+            with suppress(asyncio.CancelledError):
+                await self._rpc_dispatcher
+
+        # Cancel pending rpc_calls
+        logger.debug(f"Cancelling pending RPC calls (result awaits). Total count: {len(self._rpc_calls)}")
+        for pending_call in self._rpc_calls.values():
+            pending_call._fut.cancel()
+
+        # Disconnect websocket
+        await self._disconnect()
+
+        return True
 
     async def _connect(self):
         async with self._connect_lock:
@@ -56,28 +85,27 @@ class NerdDiaryClient:
                 except TimeoutError:
                     await asyncio.sleep(self._config.reconnect_timeout)
                     retry += 1
+                except ConnectionRefusedError:
+                    await asyncio.sleep(self._config.reconnect_timeout)
+                    retry += 1
+                except OSError:
+                    await asyncio.sleep(self._config.reconnect_timeout)
+                    retry += 1
 
             if self._ws is None and self._running:
                 # failed to reconnect in time and the client is still running
-                err = f"Failed to connect to NerdDiary server after {retry * self._config.max_connect_retries} seconds ({retry} retries)"
+                err = f"Failed to connect to NerdDiary server after {retry * self._config.reconnect_timeout} seconds ({retry} retries)"
                 logger.error(err)
                 raise ConnectionError(err)
 
+            logger.debug(
+                f"Succesfully connected to NerdDiary server at <{self._config.server_uri}>, on try #{str(retry+1)}"
+            )
+
     async def _disconnect(self):
         if self._ws is not None:
+            logger.debug(f"Disconnecting from NerdDiary server at <{self._config.server_uri}>")
             await self._ws.close()
-
-    async def start(self) -> NerdDiaryClient:
-        try:
-            asyncio.get_running_loop()
-        except RuntimeError:
-            raise RuntimeError("Start the loop before starting NerdDiary client")
-
-        logger.debug("Starting NerdDiary client")
-        self._running = True
-        await self._connect()
-        self._rpc_dispatcher = asyncio.create_task(self._rpc_dispatch())
-        return self
 
     async def run_rpc(
         self,
@@ -95,7 +123,7 @@ class NerdDiaryClient:
         except ConnectionClosedOK:
             await self._connect()
         except ConnectionClosedError:
-            err = "NerdDiary server connection terminated with an error"
+            err = "NerdDiary server connection terminated with an error. Exiting RPC call handler"
             logger.error(err)
             raise ConnectionError(err)
 
@@ -107,23 +135,24 @@ class NerdDiaryClient:
             del self._rpc_calls[id]
             return res
         except asyncio.CancelledError:
-            pass
+            del self._rpc_calls[id]
 
     async def _rpc_dispatch(self):
         while self._running:
             try:
                 # TODO: this doesn't support batch calls (when incoming is a list of dicts)
-                raw_response = json.loads(await self._ws.recv())
-                if "method" in raw_response:
+                raw_response = await self._ws.recv()
+                parsed_response = json.loads(raw_response)
+                if "method" in parsed_response:
                     # Execute local method (from RPC call)
                     logger.debug(
-                        f"Processing incoming RPC call from the server. Method <{raw_response['method']}> with params <{raw_response['params']}>. JSON RPC id: {raw_response['id']}"
+                        f"Processing incoming RPC call from the server. Method <{parsed_response['method']}> with params <{parsed_response['params']}>. JSON RPC id: {parsed_response['id']}"
                     )
-                    if response := await async_dispatch(raw_response):
+                    if response := await async_dispatch(raw_response, context=self):
                         await self._ws.send(response)
                 else:
                     # Process RPC call response
-                    match parse(raw_response):
+                    match parse(parsed_response):
                         case Ok(result, id):
                             logger.debug(
                                 f"Processing RPC call response from the server. Result <{result}>. JSON RPC id: {id}"
@@ -135,50 +164,35 @@ class NerdDiaryClient:
             except ConnectionClosedOK:
                 await self._connect()
             except ConnectionClosedError:
-                err = "NerdDiary server connection terminated with an error"
+                err = "NerdDiary server connection terminated with an error. Exiting rpc dispatcher"
                 logger.error(err)
                 raise ConnectionError(err)
-
-    async def close(self):
-        logger.debug("Closing NerdDiary client")
-        if self._running:
-            # Stop any internal loops
-            self.stop()
-
-        # If rpc dispatcher exist, wait for it to stop
-        if self._rpc_dispatcher:
-            logger.debug("Waiting for RPC dispatcher to gracefully finish")
-            await self._rpc_dispatcher
-
-        # Cancel pending rpc_calls
-        logger.debug(f"Cancelling pending RPC calls (result awaits). Total count: {len(self._rpc_calls)}")
-        for pending_call in self._rpc_calls.values():
-            pending_call._fut.cancel()
-
-        # Disconnect websocket
-        await self._disconnect()
+            except asyncio.CancelledError:
+                break
 
     def stop(self):
         self._running = False
 
-    async def __aenter__(self) -> NerdDiaryClient:
-        return await self.start()
-
-    async def __aexit__(self, exc_type, exc_value, traceback):
-        if exc_type:
-            logger.error("Exception caught before client was closed", exc_info=(exc_type, exc_value, traceback))
-
-        await self.close()
+    @method  # type:ignore
+    async def ping(self, p) -> Result:
+        print("Ndc ws called from the server with param: " + str(p))
+        return Success("pong " + str(p))
 
 
 if __name__ == "__main__":
-    logger.setLevel("DEBUG")
+    import sys
+    from time import sleep
 
-    async def main():
-        try:
-            ndc = await NerdDiaryClient().start()
-            print(await ndc.run_rpc("ping", params={"p": "1"}))
-        except KeyboardInterrupt:
-            await ndc.close()
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(logging.StreamHandler(stream=sys.stdout))
 
-    asyncio.run(main())
+    with NerdDiaryClient() as ndc:
+        for i in range(10):
+            try:
+                print(ndc.loop.run_until_complete(ndc.run_rpc("ping", params={"p": str(i)})))
+            except RuntimeError as e:
+                print(str(e))
+            except ConnectionError as e:
+                print(str(e))
+                break
+            sleep(1)
