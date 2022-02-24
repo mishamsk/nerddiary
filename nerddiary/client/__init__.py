f6b06404193636c9ea2069511fe6c03c727f80e1
mishamsk <mishamsk@gmail.com>
2022-02-23 23:47:40 -0500

    ✨ expose necessary modules from client

--- nerddiary/client/__init__.py
+++ nerddiary/client/__init__.py
@@ -1,5 +1,13 @@
-from ..server.schema import NotificationType, UserSessionSchema
+from ..server.rpc import RPCErrors
+from ..server.schema import NotificationType, PollBaseSchema, UserSessionSchema
 from ..server.session.status import UserSessionStatus
 from .client import NerdDiaryClient
 
-__all__ = ["NotificationType", "UserSessionSchema", "UserSessionStatus", "NerdDiaryClient"]
+__all__ = [
+    "NotificationType",
+    "UserSessionSchema",
+    "PollBaseSchema",
+    "UserSessionStatus",
+    "NerdDiaryClient",
+    "RPCErrors",
+]
